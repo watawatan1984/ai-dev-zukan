6c5cee8a967b656a3eeb52951dd7b55ad7da5fb9
watawatan1984 <watanabe.y1984@gmail.com>
2026-08-30 13:40:20 +0900

    test: target accessible catalog search field

--- test/system/public_catalog_test.rb
+++ test/system/public_catalog_test.rb
@@ -5,9 +5,9 @@ class PublicCatalogTest < ApplicationSystemTestCase
     visit root_path
 
     assert_text "次に使うMCP・Skill・技術記事を、"
-    assert_field "リソースを検索"
+    search_field = find('input[aria-label="リソースを検索"]')
 
-    fill_in "リソースを検索", with: "GitHub"
+    search_field.fill_in with: "GitHub"
     click_on "検索"
 
     assert_current_path resources_path, ignore_query: true
